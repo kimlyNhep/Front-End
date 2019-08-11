4383ab2f5294d8f7f33c89c728c72aac13816af8
Kimly Nhep <kimly.nhep0168@gmail.com>
2019-08-12 01:12:57 +0700

    js

---
+++ JS/Js-Basic/script.js
@@ -0,0 +1,17 @@
+console.log("Hello World");
+
+var year, yearJohn, yearMark;
+now = 2018;
+ageJohn = 1990;
+ageMark = 1998;
+
+//Logical operator
+var yearJohn = now - ageJohn;
+var yearMark = now - ageMark;
+
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
