21b42b4f358289b935043e44f58133fe56dad371
Kimly Nhep <kimly.nhep0168@gmail.com>
2019-09-16 23:27:24 +0700

    done

--- JS/Closures/script.js
+++ JS/Closures/script.js
@@ -1,5 +1,7 @@
+//////// Lecture: Closures//////////////////////
+
 function retirement(retirementAge) {
-  var a = " years left until retirement";
+  var a = " years left until retirment.";
   return function(yearOfBirth) {
     var age = 2019 - yearOfBirth;
     console.log(retirementAge - age + a);
@@ -9,4 +11,20 @@ function retirement(retirementAge) {
 var retirementUS = retirement(66);
 retirementUS(1998);
 
-retirement(66)(1998);
+//retirement(66)(1998);
+
+///Exercise Closures
+
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === "designer") {
+      console.log(name + ", can you please explain what UX design is?");
+    } else if (job === "teacher") {
+      console.log("What subject do you teach, " + name + "?");
+    } else {
+      console.log("Hello " + name + ", what do you do?");
+    }
+  };
+}
+
+interviewQuestion("designer")("kimly");
